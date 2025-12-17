4d4ba9301e7fecad63193fa57a8a7dc8d4952b42
Ayush5304 <mishrayush0503@gmail.com>
2025-12-17 12:07:30 +0530

    [Ayush5304] Display Harry Age java code

---
+++ gcr-codebase/programming-elements/Level1/DisplayHarryAge.java
@@ -0,0 +1,8 @@
+class DisplayHarryAge{
+public static void main(    String[] args){
+int birth_year=2000;
+int curr_year=2024;
+int age= curr_year - birth_year;
+System.out.println("Age Of Harry is: " + age);
+}
+}
