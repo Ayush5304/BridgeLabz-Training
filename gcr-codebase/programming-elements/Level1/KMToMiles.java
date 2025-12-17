45531cf5307a284c96e07c67e028ed6351b8b892
Ayush5304 <mishrayush0503@gmail.com>
2025-12-17 12:43:00 +0530

    [Ayush5304] KMToMiles java code

---
+++ gcr-codebase/programming-elements/Level1/KMToMiles.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class KMToMiles{
+public static void main( String [] args){
+Scanner sc=new Scanner(System.in);
+double kilometers=10.8;
+double miles=10.8 *1.6;
+System.out.println(" The distance  10.8 km in miles is: " + miles);
+}
+}
