dc2885c96acaed60aa0c410db152451830678247
Ayush5304 <mishrayush0503@gmail.com>
2025-12-17 16:15:22 +0530

    [Ayush5304] SquareSide java code

---
+++ gcr-codebase/programming-elements/Level1/SquareSide.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+//program for finding a side of square//
+    public class SquareSide {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        //parameter you read from user and take side of (sqaure=perimeter/4) //
+        System.out.print("Enter the perimeter of square ");
+        double perimeter= sc.nextDouble();
+		double side= perimeter/4;
+		//output//
+		System.out.println("The length of the side is:" + side + "whose perimeter is :"+ perimeter);
+		sc.close();
+		}
+		}
