b594cd9f0c883b8b175e32b86af22332caead63c
Ayush Mishra <148375179+Ayush5304@users.noreply.github.com>
2025-12-17 21:27:08 +0530

    [Ayush5304] KMToMiles java code

--- gcr-codebase/programming-elements/Level1/KMToMiles.java
+++ gcr-codebase/programming-elements/Level1/KMToMiles.java
@@ -1,9 +1,13 @@
+// Program to convert kilometers to miles
 import java.util.*;
 public class KMToMiles{
 public static void main( String [] args){
-Scanner sc=new Scanner(System.in);
-double kilometers=10.8;
-double miles=10.8 *1.6;
-System.out.println(" The distance  10.8 km in miles is: " + miles);
+   Scanner sc=new Scanner(System.in);
+   // Distance in kilometers
+   double kilometers=10.8;
+  // Conversion formula (1 mile = 1.6 km)
+  double miles=10.8 *1.6;
+    // Output
+   System.out.println(" The distance  10.8 km in miles is: " + miles);
 }
 }
