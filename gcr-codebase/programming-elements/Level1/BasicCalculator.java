aead685e1cea83e994903071e409fa5e747d1057
Ayush5304 <mishrayush0503@gmail.com>
2025-12-17 15:55:54 +0530

    [Ayush5304]  BasicCalculator java code

---
+++ gcr-codebase/programming-elements/Level1/BasicCalculator.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Taking user input//
+        System.out.print("Enter first number");
+        double numberOne = sc.nextDouble();
+
+        System.out.print("Enter second number");
+        double numberTwo= sc.nextDouble();
+
+        // Arithmetic operations on two input numbers//
+        double addition = numberOne + numberTwo;
+        double subtraction = numberOne - numberTwo;
+        double multiplication = numberOne * numberTwo;
+        double division = numberOne/ numberTwo;
+
+        // Output//
+        System.out.println(
+            "The addition, subtraction, multiplication and division value of 2 numbers "
+            + numberOne+ " and " + numberTwo + " is "
+            + addition + ", " + subtraction + ", "
+            + multiplication + ", and " + division );
+
+        sc.close();
+    }
+}
+
+ 
