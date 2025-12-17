d0b67d84981dffec119a48eb5ac0c54782dbc769
Ayush5304 <mishrayush0503@gmail.com>
2025-12-17 12:29:20 +0530

    [Ayush5304]  AVGMarks java code

---
+++ gcr-codebase/programming-elements/Level1/AVGMarks.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class AVGMarks{
+public static void main( String [] args){
+int maths_marks=94;
+int physics_marks=95;
+int chemistry_marks=96;
+double AVG_Marks=((double) maths_marks+(double)  physics_marks +(double) chemistry_marks)/3;
+System.out.println("Sams AVG_Marks is : "+ AVG_Marks);
+}
+} 
