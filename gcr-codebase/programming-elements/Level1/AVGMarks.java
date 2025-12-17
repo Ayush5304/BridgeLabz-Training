8d5745600c8941cbe7dd136d31fd55c627f25579
Ayush Mishra <148375179+Ayush5304@users.noreply.github.com>
2025-12-17 21:21:27 +0530

    [Ayush5304] AVGMarks java code

--- gcr-codebase/programming-elements/Level1/AVGMarks.java
+++ gcr-codebase/programming-elements/Level1/AVGMarks.java
@@ -1,10 +1,14 @@
+//Program to calculate average percentage of PCM marks
 import java.util.*;
 public class AVGMarks{
 public static void main( String [] args){
-int maths_marks=94;
-int physics_marks=95;
-int chemistry_marks=96;
-double AVG_Marks=((double) maths_marks+(double)  physics_marks +(double) chemistry_marks)/3;
-System.out.println("Sams AVG_Marks is : "+ AVG_Marks);
+  //  Marks in subjects
+   int maths_marks=94;
+   int physics_marks=95;
+   int chemistry_marks=96;
+  // Calculate average
+   double AVG_Marks=((double) maths_marks+(double)  physics_marks +(double) chemistry_marks)/3;
+  // output
+  System.out.println("Sams AVG_Marks is : "+ AVG_Marks);
 }
 } 
