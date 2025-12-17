beeb9d4510699f7a7b1a0597a78311bdc25c646e
Ayush Mishra <148375179+Ayush5304@users.noreply.github.com>
2025-12-17 21:41:40 +0530

    [Ayush5304]  HeightInInchesAndFeet java code

--- gcr-codebase/programming-elements/Level1/HeightInInchesAndFeet.java
+++ gcr-codebase/programming-elements/Level1/HeightInInchesAndFeet.java
@@ -1,11 +1,15 @@
+//program to convert height from cm to   inches and feet
 import java.util.*;
 public class HeightInInchesAndFeet{
 public static void main( String [] args){
-   Scanner sc=new Scanner(System.in);
-   System.out.print("height in centimeters");
-   double centimeters=sc.nextDouble();
-   double InInches= centimeters/2.54;
-   int feet =(int)InInches/12;
-   System.out.println( "Your Height in cm is:" + centimeters + "while in feet is :"+ feet+ "and inches is:" +InInches);
+    Scanner sc=new Scanner(System.in);
+ // Height in centimeters
+    System.out.print("height in centimeters");
+    double centimeters=sc.nextDouble();
+    double InInches= centimeters/2.54;
+// Remaining inches
+    int feet =(int)InInches/12;
+// Output
+    System.out.println( "Your Height in cm is:" + centimeters + "while in feet is :"+ feet+ "and inches is:" +InInches);
    }
    }
