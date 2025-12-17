7c77588a5a983184510483d77ba8724e1cae9140
Ayush5304 <mishrayush0503@gmail.com>
2025-12-17 14:10:18 +0530

    [Ayush5304]  ProfitLoss java code

---
+++ gcr-codebase/programming-elements/Level1/ProfitLoss.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class ProfitLoss{
+public static void main( String [] args){
+double costPrice=129;
+double sellingPrice=191;
+double Profit=sellingPrice-costPrice;
+double profitPercentage = Profit / costPrice * 100;
+System.out.println("The Cost Price is INR 129 and Selling Price is INR 191"+
+"The Profit is INR :"+ Profit+" and the Profit Percentage is:" +profitPercentage);
+}
+}
