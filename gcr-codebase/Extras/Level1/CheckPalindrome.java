7d6ebf7f2aa9502fb499402a8365ece63edeb26b
Ayush5304 <mishrayush0503@gmail.com>
2025-12-23 15:47:16 +0530

    [Ayush5304] CheckPalindrome java code

---
+++ gcr-codebase/Extras/Level1/CheckPalindrome.java
@@ -0,0 +1,42 @@
+
+
+//a Java program to check if a given string is a palindrome (a string that reads thesame forward and backward)
+
+import java.util.Scanner;
+
+// make a class checkpalindrome
+public class CheckPalindrome{
+public static boolean palindromeOrNot(String input){
+          String result="";
+		  
+	// take a for loop to store each character 
+        for(int i=input.length()-1; i>=0; i--){
+        result+= input.charAt(i);
+		}
+       if (result.equals(input)){
+		   return true;
+	   }
+ 
+		   return false;
+	   
+}
+public static void main (String [] args){
+Scanner sc= new Scanner(System.in);
+
+// take a user input
+System.out.println("enter a string");
+String input = sc.nextLine();
+      
+	  //make a method to call 
+	  boolean CP = palindromeOrNot(input);
+	   
+	   // display the output
+	   if(CP==true){
+	   System.out.println("string is a palindrome");
+	   }
+	   else{
+		    System.out.println("string is not a palindrome");
+		
+		}
+}
+}
