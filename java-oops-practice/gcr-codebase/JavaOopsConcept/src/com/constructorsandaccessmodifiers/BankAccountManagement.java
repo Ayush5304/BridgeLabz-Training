ec61807b2a3b54eb3a7212393a6c50048cb86d5f
Ayush5304 <mishrayush0503@gmail.com>
2025-12-30 16:11:32 +0530

    [Ayush5304]  BankAccountManagement java code

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/constructorsandaccessmodifiers/BankAccountManagement.java
@@ -0,0 +1,24 @@
+package com.constructorsandaccessmodifiers;
+
+
+//Main class
+public class BankAccountManagement {
+
+public static void main(String[] args) {
+
+   SavingsAccount account =
+           new SavingsAccount("SB1001", "Ayush Mishra", 5000.0, 4.5);
+
+   account.displayAccountDetails();
+
+   System.out.println("\nDepositing ₹2000...\n");
+   account.deposit(2000);
+
+   account.displayAccountDetails();
+
+   System.out.println("\nWithdrawing ₹3000...\n");
+   account.withdraw(3000);
+
+   account.displayAccountDetails();
+}
+}
