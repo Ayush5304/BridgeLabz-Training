7cb2486ae5b80eefdfb2ba29ba06ded4c6dd5fe5
Ayush5304 <mishrayush0503@gmail.com>
2025-12-30 22:10:12 +0530

    [Ayush5304]  ListNode java code

---
+++ leet-code-codebase/Day4/ListNode.java
@@ -0,0 +1,55 @@
+public class RemoveDuplicatesFromSortedList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+
+        ListNode current = head;
+
+        while (current != null && current.next != null) {
+
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+
+    // Helper method to print linked list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        // Create list: 1 -> 1 -> 2 -> 3 -> 3
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+
+        System.out.print("Original List: ");
+        printList(head);
+
+        head = deleteDuplicates(head);
+
+        System.out.print("After Removing Duplicates: ");
+        printList(head);
+    }
+}
