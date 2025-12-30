8056bb6c6a48a0a96e45c3d19e816fbe226c84b5
Ayush5304 <mishrayush0503@gmail.com>
2025-12-30 21:58:17 +0530

    [Ayush5304]  LengthOfLastWord java code

---
+++ leet-code-codebase/Day3/LengthOfLastWord.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class LengthOfLastWord{
+
+    public static int lengthOfLastWord(String s) {
+        int length = 0;
+        int i = s.length() - 1;
+
+        // Skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // Count characters of last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+
+        int result = lengthOfLastWord(s);
+        System.out.println("Length of last word: " + result);
+
+        sc.close();
+    }
+}
