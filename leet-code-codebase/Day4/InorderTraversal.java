3289fe538bf5584fc6cb8d358597c9f75b840829
Ayush5304 <mishrayush0503@gmail.com>
2025-12-30 22:09:32 +0530

    [Ayush5304] InorderTraversal.java

---
+++ leet-code-codebase/Day4/InorderTraversal.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class InorderTraversal {
+
+    static class TreeNode {
+        int val;
+        TreeNode left, right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
+    }
+
+    private static void inorder(TreeNode node, List<Integer> result) {
+        if (node == null) return;
+
+        inorder(node.left, result);
+        result.add(node.val);
+        inorder(node.right, result);
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+
+        System.out.println("Inorder Traversal: " + inorderTraversal(root));
+    }
+}
