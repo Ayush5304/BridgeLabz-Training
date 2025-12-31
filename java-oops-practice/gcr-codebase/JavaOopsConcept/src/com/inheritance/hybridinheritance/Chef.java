71f3bb1b05a8ef9348bcad730207066e8d5de6a7
Ayush5304 <mishrayush0503@gmail.com>
2025-12-31 16:25:31 +0530

    [Ayush5304] Chef java code

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/inheritance/hybridinheritance/Chef.java
@@ -0,0 +1,13 @@
+package com.inheritance.hybridinheritance;
+
+public class Chef extends Person implements Worker {
+
+    Chef(String name, int id) {
+        super(name, id);
+    }
+
+    @Override
+    public void performDuties() {
+        System.out.println(name + " is cooking food 👨‍🍳");
+    }
+}
