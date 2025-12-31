61c8fffc2dd0baecf939eaf3f4f029dfb455d7b1
Ayush5304 <mishrayush0503@gmail.com>
2025-12-31 15:56:29 +0530

    [Ayush5304]   SmartHomeTest java code

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/inheritance/singleinheritance/SmartHomeTest.java
@@ -0,0 +1,10 @@
+package com.inheritance.singleinheritance;
+
+public class SmartHomeTest {
+
+    public static void main(String[] args) {
+
+        Thermostat t1 = new Thermostat("TH-101", true, 24);
+        t1.displayStatus();
+    }
+}
