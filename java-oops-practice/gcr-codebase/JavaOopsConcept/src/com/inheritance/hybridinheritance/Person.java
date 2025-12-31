1c2b4b7e9d96f0f96c63fa8da29fb9eab9b1463c
Ayush5304 <mishrayush0503@gmail.com>
2025-12-31 16:26:29 +0530

    [Ayush5304]Person java code

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/inheritance/hybridinheritance/Person.java
@@ -0,0 +1,12 @@
+package com.inheritance.hybridinheritance;
+
+public class Person {
+
+    String name;
+    int id;
+
+    Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
