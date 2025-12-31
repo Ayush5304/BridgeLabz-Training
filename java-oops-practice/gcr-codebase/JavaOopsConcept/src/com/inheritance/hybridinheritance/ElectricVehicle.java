59147c4b8c4c2e9a1cbccc65a83359ce12a5e1c5
Ayush5304 <mishrayush0503@gmail.com>
2025-12-31 16:26:04 +0530

    [Ayush5304] ElectricVehicle java code

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/inheritance/hybridinheritance/ElectricVehicle.java
@@ -0,0 +1,12 @@
+package com.inheritance.hybridinheritance;
+
+public class ElectricVehicle extends Vehicle {
+
+    ElectricVehicle(int maxSpeed, String model) {
+        super(maxSpeed, model);
+    }
+
+    void charge() {
+        System.out.println(model + " is charging 🔋");
+    }
+}
