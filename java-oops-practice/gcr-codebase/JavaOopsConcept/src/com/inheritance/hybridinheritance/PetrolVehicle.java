adfafe0554dd775aa3a6014d7b058c32ee29941a
Ayush5304 <mishrayush0503@gmail.com>
2025-12-31 16:26:51 +0530

    [Ayush5304] PetrolVehicle java  code

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/inheritance/hybridinheritance/PetrolVehicle.java
@@ -0,0 +1,13 @@
+package com.inheritance.hybridinheritance;
+
+public class PetrolVehicle extends Vehicle implements Refuelable {
+
+    PetrolVehicle(int maxSpeed, String model) {
+        super(maxSpeed, model);
+    }
+
+    @Override
+    public void refuel() {
+        System.out.println(model + " is refueling ");
+    }
+}
