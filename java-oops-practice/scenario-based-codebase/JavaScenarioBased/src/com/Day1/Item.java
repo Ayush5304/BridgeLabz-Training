eebd7f3a456625a07d9f0e327957663c731c9355
Ayush5304 <mishrayush0503@gmail.com>
2026-01-01 12:25:12 +0530

    [Ayush5304]  Item java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day1/Item.java
@@ -0,0 +1,11 @@
+package com.Day1;
+
+public class Item {
+    String name;
+    double pricePerUnit;
+
+    Item(String name, double pricePerUnit) {
+        this.name = name;
+        this.pricePerUnit = pricePerUnit;
+    }
+}
