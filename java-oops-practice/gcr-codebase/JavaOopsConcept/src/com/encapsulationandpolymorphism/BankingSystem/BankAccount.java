77de5f7cfbb9ffd5532d7de1ddeaeafab0e7c4c0
Ayush5304 <mishrayush0503@gmail.com>
2026-01-01 13:26:50 +0530

    [Ayush5304] BankAccount java code

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcept/src/com/encapsulationandpolymorphism/BankingSystem/BankAccount.java
@@ -0,0 +1,55 @@
+package com.encapsulationandpolymorphism.BankingSystem;
+
+public abstract class BankAccount {
+
+    private String accountNumber;
+    private String holderName;
+    protected double balance;
+
+    public BankAccount(String accountNumber, String holderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    // Encapsulation (Getters)
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getHolderName() {
+        return holderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    // Concrete methods
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: ₹" + amount);
+        } else {
+            System.out.println("Invalid deposit amount");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrawn: ₹" + amount);
+        } else {
+            System.out.println("Insufficient balance or invalid amount");
+        }
+    }
+
+    // Abstract method
+    public abstract double calculateInterest();
+
+    public void displayDetails() {
+        System.out.println("Account No: " + accountNumber);
+        System.out.println("Holder: " + holderName);
+        System.out.println("Balance: ₹" + balance);
+    }
+}
