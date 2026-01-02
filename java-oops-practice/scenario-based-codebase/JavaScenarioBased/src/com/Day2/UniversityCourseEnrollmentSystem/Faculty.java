6adbe6e74efb1bfdca4ceb8d0724f3fb8dc882ca
Ayush5304 <mishrayush0503@gmail.com>
2026-01-02 14:17:43 +0530

    [Ayush5304]  Faculty java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day2/UniversityCourseEnrollmentSystem/Faculty.java
@@ -0,0 +1,14 @@
+package com.Day2.UniversityCourseEnrollmentSystem;
+
+class Faculty {
+
+    private String facultyName;
+
+    public Faculty(String facultyName) {
+        this.facultyName = facultyName;
+    }
+
+    public String getFacultyName() {
+        return facultyName;
+    }
+}
