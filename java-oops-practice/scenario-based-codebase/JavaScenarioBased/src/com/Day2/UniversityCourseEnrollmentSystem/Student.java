287e41042f34d16a2f01ab6e042a0f5560e5d9b1
Ayush5304 <mishrayush0503@gmail.com>
2026-01-02 14:18:46 +0530

    [Ayush5304]Student java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day2/UniversityCourseEnrollmentSystem/Student.java
@@ -0,0 +1,28 @@
+package com.Day2.UniversityCourseEnrollmentSystem;
+
+class Student {
+
+    private String name;
+    private int rollNo;
+    private double gpa;   
+
+    public Student(String name, int rollNo) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.gpa = 0.0;
+    }
+
+    public Student(String name, int rollNo, double initialGpa) {
+        this.name = name;
+        this.rollNo = rollNo;
+        this.gpa = initialGpa;
+    }
+
+    protected void updateGpa(double newGpa) {
+        this.gpa = newGpa;
+    }
+
+    public String getTranscript() {
+        return "Name: " + name + ", Roll No: " + rollNo + ", GPA: " + gpa;
+    }
+}
