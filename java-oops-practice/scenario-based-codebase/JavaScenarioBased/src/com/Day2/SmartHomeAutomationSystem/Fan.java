69a6aff64bd4eeec0d9bb3d0eb3bc6acef0ab101
Ayush5304 <mishrayush0503@gmail.com>
2026-01-02 14:14:28 +0530

    [Ayush5304]Fan java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day2/SmartHomeAutomationSystem/Fan.java
@@ -0,0 +1,23 @@
+package com.Day2.SmartHomeAutomationSystem;
+class Fan extends Appliance {
+
+    public Fan() {
+        super(75); 
+    }
+
+    public Fan(int power) {
+        super(power);
+    }
+
+    @Override
+    public void turnOn() {
+        setOn(true);
+        System.out.println("Fan started (" + getPowerConsumption() + "W)");
+    }
+
+    @Override
+    public void turnOff() {
+        setOn(false);
+        System.out.println("Fan stopped");
+    }
+}
