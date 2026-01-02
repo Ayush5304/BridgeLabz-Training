e6de2ab09bf864d3cf5dbd0230a8fc092366c6f9
Ayush5304 <mishrayush0503@gmail.com>
2026-01-02 14:23:06 +0530

    [Ayush5304]Customer java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day2/VehicleRentalApplication/Customer.java
@@ -0,0 +1,14 @@
+package com.Day2.VehicleRentalApplication;
+
+class Customer {
+
+    private String name;
+
+    public Customer(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
