da81b785fe12f6c8a1d6677e1b6ebca8fa7cc962
Ayush5304 <mishrayush0503@gmail.com>
2026-01-02 14:18:25 +0530

    [Ayush5304] PostGraduate java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day2/UniversityCourseEnrollmentSystem/PostGraduate.java
@@ -0,0 +1,8 @@
+package com.Day2.UniversityCourseEnrollmentSystem;
+
+class Postgraduate extends Student {
+
+    public Postgraduate(String name, int rollNo, double gpa) {
+        super(name, rollNo, gpa);
+    }
+}
