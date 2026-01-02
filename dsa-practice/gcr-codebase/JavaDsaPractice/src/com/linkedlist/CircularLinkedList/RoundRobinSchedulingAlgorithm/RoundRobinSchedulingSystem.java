b4dea602dd16f2af935907a0488b7ba17f44d65f
Ayush5304 <mishrayush0503@gmail.com>
2026-01-02 16:20:18 +0530

    [Ayush5304]   RoundRobinSchedulingSystem java code

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/linkedlist/CircularLinkedList/RoundRobinSchedulingAlgorithm/RoundRobinSchedulingSystem.java
@@ -0,0 +1,15 @@
+package com.linkedlist.CircularLinkedList.RoundRobinSchedulingAlgorithm;
+
+public class RoundRobinSchedulingSystem {
+
+    public static void main(String[] args) {
+
+        RoundRobinScheduler scheduler = new RoundRobinScheduler(3);
+
+        scheduler.addProcess(1, 10, 1);
+        scheduler.addProcess(2, 5, 2);
+        scheduler.addProcess(3, 8, 1);
+
+        scheduler.simulate();
+    }
+}
