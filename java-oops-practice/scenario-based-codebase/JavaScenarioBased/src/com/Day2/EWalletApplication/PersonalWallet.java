fd86ef30100ecb22ac77a336a05739913881d486
Ayush5304 <mishrayush0503@gmail.com>
2026-01-02 13:22:18 +0530

    [Ayush5304] PersonalWallet java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day2/EWalletApplication/PersonalWallet.java
@@ -0,0 +1,32 @@
+package com.Day2.EWalletApplication;
+class PersonalWallet extends Wallet {
+
+    private static final double TRANSFER_LIMIT = 10000;
+
+    public PersonalWallet(double balance) {
+        super(balance);
+    }
+
+    public PersonalWallet(double balance, double referralBonus) {
+        super(balance, referralBonus);
+    }
+
+    @Override
+    public void transferTo(User receiver, double amount) {
+
+        if (amount > TRANSFER_LIMIT) {
+            System.out.println("Transfer limit exceeded (Personal Wallet)");
+            return;
+        }
+
+        if (getBalance() < amount) {
+            System.out.println("Insufficient balance");
+            return;
+        }
+
+        debit(amount);
+        receiver.getWallet().credit(amount);
+
+        System.out.println("Transferred ₹" + amount + " to " + receiver.getName());
+    }
+}
