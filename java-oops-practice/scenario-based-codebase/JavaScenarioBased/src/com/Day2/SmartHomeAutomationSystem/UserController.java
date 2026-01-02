91b973fb9d5f59a2ded5cc7816d6ea0815a21a3a
Ayush5304 <mishrayush0503@gmail.com>
2026-01-02 14:15:42 +0530

    [Ayush5304] UserController java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day2/SmartHomeAutomationSystem/UserController.java
@@ -0,0 +1,21 @@
+package com.Day2.SmartHomeAutomationSystem;
+class UserController {
+
+    public void controlDevice(Controllable device, boolean power) {
+        if (power) {
+            device.turnOn();
+        } else {
+            device.turnOff();
+        }
+    }
+
+    public void compareEnergy(Appliance a1, Appliance a2) {
+        if (a1.getPowerConsumption() > a2.getPowerConsumption()) {
+            System.out.println("First appliance consumes more energy");
+        } else if (a1.getPowerConsumption() < a2.getPowerConsumption()) {
+            System.out.println("Second appliance consumes more energy");
+        } else {
+            System.out.println("Both appliances consume equal energy");
+        }
+    }
+}
