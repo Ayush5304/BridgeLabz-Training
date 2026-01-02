2a93a2d53db696a229bb8dfc2d91b6caf9efb5cc
Ayush5304 <mishrayush0503@gmail.com>
2026-01-02 16:30:25 +0530

    [Ayush5304]    UserNode java code

---
+++ dsa-practice/gcr-codebase/JavaDsaPractice/src/com/linkedlist/Singlylinkedlist/SocialMediaFriendConnections/UserNode.java
@@ -0,0 +1,17 @@
+package com.linkedlist.Singlylinkedlist.SocialMediaFriendConnections;
+
+class UserNode {
+    int userId;
+    String name;
+    int age;
+    FriendNode friends;   // head of friend list
+    UserNode next;
+
+    public UserNode(int userId, String name, int age) {
+        this.userId = userId;
+        this.name = name;
+        this.age = age;
+        this.friends = null;
+        this.next = null;
+    }
+}
