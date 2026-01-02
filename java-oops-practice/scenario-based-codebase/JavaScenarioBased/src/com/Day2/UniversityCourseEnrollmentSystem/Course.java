a414db08e24b9d0a85bca302476f088dcdeb49e1
Ayush5304 <mishrayush0503@gmail.com>
2026-01-02 14:16:41 +0530

    [Ayush5304] Course java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day2/UniversityCourseEnrollmentSystem/Course.java
@@ -0,0 +1,14 @@
+package com.Day2.UniversityCourseEnrollmentSystem;
+
+class Course {
+
+    private String courseName;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+}
