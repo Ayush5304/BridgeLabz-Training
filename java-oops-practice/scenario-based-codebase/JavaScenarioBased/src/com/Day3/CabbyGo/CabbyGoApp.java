520de8b5c3c94c18da2ff0b866b03856939d6749
Ayush5304 <mishrayush0503@gmail.com>
2026-01-03 12:59:20 +0530

    [Ayush5304] CabbyGoApp java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day3/CabbyGo/CabbyGoApp.java
@@ -0,0 +1,12 @@
+package com.Day3.CabbyGo;
+public class CabbyGoApp {
+    public static void main(String[] args) {
+
+        Vehicle vehicle = new Sedan("MP09-1234"); // polymorphism
+        Driver driver = new Driver("Rahul", "DL12345", 4.8);
+
+        IRideService rideService = new RideService();
+        rideService.bookRide(vehicle, driver, 12); // distance in km
+        rideService.endRide();
+    }
+}
