53b37350d5ca57489c3f85cd2edf34e90e64d24f
Ayush5304 <mishrayush0503@gmail.com>
2026-01-05 15:56:49 +0530

    [Ayush5304] SwiftCartDemo java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day4/SwiftCart/SwiftCartDemo.java
@@ -0,0 +1,15 @@
+package com.Day4.SwiftCart;
+
+public class SwiftCartDemo {
+    public static void main(String[] args) {
+
+        Product milk = new PerishableProduct("Milk", 60);
+        Product rice = new NonPerishableProduct("Rice", 100);
+
+        Cart cart = new Cart();
+        cart.addProduct(milk);
+        cart.addProduct(rice);
+
+        cart.generateBill();
+    }
+}
