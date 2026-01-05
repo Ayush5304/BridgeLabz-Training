205459c19e48e7947ce9d9ecaf1bab5b92e79e5a
Ayush5304 <mishrayush0503@gmail.com>
2026-01-05 15:51:05 +0530

    [Ayush5304] Faculty java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day4/CampusConnect/Faculty.java
@@ -0,0 +1,13 @@
+package com.Day4.CampusConnect;
+
+public class Faculty extends Person {
+
+	Faculty(int id, String name, String email){
+		super(id, name, email);
+	
+	}
+@Override
+void printDetails() {
+	System.out.println("Faculty"+ name+ "Email"+ email);
+}
+}
