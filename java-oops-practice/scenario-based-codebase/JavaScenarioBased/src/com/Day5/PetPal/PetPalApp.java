490adaaff5515df11ed7da97931fc95dab319eb8
Ayush5304 <mishrayush0503@gmail.com>
2026-01-06 16:54:31 +0530

    [Ayush5304]   PetPalApp java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day5/PetPal/PetPalApp.java
@@ -0,0 +1,28 @@
+package com.Day5.PetPal;
+
+public class PetPalApp {
+
+    public static void main(String[] args) {
+
+        Pet dog = new Dog("Buddy");
+        Pet cat = new Cat("Whiskers");
+        Pet bird = new Bird("Tweety");
+
+        dog.makeSound();
+        dog.feed();
+        dog.play();
+        dog.showStatus();
+
+        System.out.println();
+
+        cat.makeSound();
+        cat.sleep();
+        cat.showStatus();
+
+        System.out.println();
+
+        bird.makeSound();
+        bird.play();
+        bird.showStatus();
+    }
+}
