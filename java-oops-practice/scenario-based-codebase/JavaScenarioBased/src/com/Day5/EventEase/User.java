8c29f538ec41ea3660732c6ee1a48425a9cf4134
Ayush5304 <mishrayush0503@gmail.com>
2026-01-06 16:56:07 +0530

    [Ayush5304]  User java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day5/EventEase/User.java
@@ -0,0 +1,18 @@
+package com.Day5.EventEase;
+
+class User {
+
+    private String name;
+    private String email;
+    private String phone;
+
+    public User(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
