fd7fe26ea5eb4bc9b25ab4061a4032bc9034693d
Ayush5304 <mishrayush0503@gmail.com>
2026-01-07 15:44:50 +0530

    [Ayush5304] Bike java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day6/ParkEase/Bike.java
@@ -0,0 +1,13 @@
+package com.Day6.ParkEase;
+
+class Bike extends Vehicle {
+
+    public Bike(String vehicleNumber) {
+        super(vehicleNumber, 20);
+    }
+
+    @Override
+    public double calculateCharges(int hours) {
+        return baseRate * hours;
+    }
+}
