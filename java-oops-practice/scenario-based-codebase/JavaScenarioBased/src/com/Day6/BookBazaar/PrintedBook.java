3412d50fc513fdcc03222767182fcd87bb35395c
Ayush5304 <mishrayush0503@gmail.com>
2026-01-07 15:43:39 +0530

    [Ayush5304]  PrintedBook java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day6/BookBazaar/PrintedBook.java
@@ -0,0 +1,13 @@
+package com.Day6.BookBazaar;
+
+class PrintedBook extends Book {
+
+    public PrintedBook(String title, String author, double price, int stock) {
+        super(title, author, price, stock);
+    }
+
+    @Override
+    public double applyDiscount(double totalPrice) {
+        return totalPrice * 0.05; 
+    }
+}
