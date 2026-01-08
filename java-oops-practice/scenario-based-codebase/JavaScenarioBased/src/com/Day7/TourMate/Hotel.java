7d139ef9f499a6a23bae9f30cd8b7576921e60fa
Ayush5304 <mishrayush0503@gmail.com>
2026-01-08 14:18:23 +0530

    [Ayush5304] Hotel java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day7/TourMate/Hotel.java
@@ -0,0 +1,14 @@
+package com.Day7.TourMate;
+
+public class Hotel {
+
+    private double cost;
+
+    public Hotel(double cost) {
+        this.cost = cost;
+    }
+
+    double getCost() {
+        return cost;
+    }
+}
