ce8680dc9c3cedd415ce6ecb5f8de49e08ae3cb7
Ayush5304 <mishrayush0503@gmail.com>
2026-01-08 14:22:30 +0530

    [Ayush5304] Injection java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day7/MediStore/Injection.java
@@ -0,0 +1,15 @@
+package com.Day7.MediStore;
+
+import java.time.LocalDate;
+
+public class Injection extends Medicine {
+
+    public Injection(String name, double price, LocalDate expiryDate) {
+        super(name, price, expiryDate);
+    }
+
+    @Override
+    public boolean checkExpiry() {
+        return getExpiryDate().isBefore(LocalDate.now());
+    }
+}
