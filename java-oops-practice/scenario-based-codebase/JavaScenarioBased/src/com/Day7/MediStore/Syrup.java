05b4b07c2878cacd6f82898a9631a867237f64b9
Ayush5304 <mishrayush0503@gmail.com>
2026-01-08 14:23:32 +0530

    [Ayush5304]  Syrup java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day7/MediStore/Syrup.java
@@ -0,0 +1,15 @@
+package com.Day7.MediStore;
+
+import java.time.LocalDate;
+
+public class Syrup extends Medicine {
+
+    public Syrup(String name, double price, LocalDate expiryDate) {
+        super(name, price, expiryDate);
+    }
+
+    @Override
+    public boolean checkExpiry() {
+        return getExpiryDate().isBefore(LocalDate.now());
+    }
+}
