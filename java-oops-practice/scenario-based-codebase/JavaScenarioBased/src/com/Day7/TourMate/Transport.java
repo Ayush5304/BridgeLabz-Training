5daec461c95874f55183ea58a7ac31e9ccb115b6
Ayush5304 <mishrayush0503@gmail.com>
2026-01-08 14:21:09 +0530

    [Ayush5304]  Transport java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day7/TourMate/Transport.java
@@ -0,0 +1,14 @@
+package com.Day7.TourMate;
+
+public class Transport {
+
+    private double cost;
+
+    public Transport(double cost) {
+        this.cost = cost;
+    }
+
+    double getCost() {
+        return cost;
+    }
+}
