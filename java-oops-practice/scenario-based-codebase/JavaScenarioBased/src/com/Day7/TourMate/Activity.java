cdfc923b82213a3bf4975882c7d1cc910be2e369
Ayush5304 <mishrayush0503@gmail.com>
2026-01-08 14:17:38 +0530

    [Ayush5304] Activity java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day7/TourMate/Activity.java
@@ -0,0 +1,14 @@
+package com.Day7.TourMate;
+
+public class Activity {
+
+    private double cost;
+
+    public Activity(double cost) {
+        this.cost = cost;
+    }
+
+    double getCost() {
+        return cost;
+    }
+}
