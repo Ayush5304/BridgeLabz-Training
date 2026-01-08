81006bd1c425ccea5361e22e71ac118f1d718336
Ayush5304 <mishrayush0503@gmail.com>
2026-01-08 14:20:02 +0530

    [Ayush5304] Trip java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day7/TourMate/Trip.java
@@ -0,0 +1,33 @@
+package com.Day7.TourMate;
+
+public abstract class Trip implements IBookable {
+
+    protected String destination;
+    protected int duration;
+    protected double budget;
+
+    protected Transport transport;
+    protected Hotel hotel;
+    protected Activity activity;
+
+    public Trip(String destination, int duration,Transport transport, Hotel hotel, Activity activity) {
+
+        this.destination = destination;
+        this.duration = duration;
+        this.transport = transport;
+        this.hotel = hotel;
+        this.activity = activity;
+
+        this.budget = calculateTotalBudget();
+    }
+
+    private double calculateTotalBudget() {
+        return transport.getCost()
+                + hotel.getCost()
+                + activity.getCost();
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+}
