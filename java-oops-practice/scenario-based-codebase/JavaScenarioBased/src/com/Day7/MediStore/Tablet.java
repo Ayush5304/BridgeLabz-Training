56dd8ccaee348fc6440d188cd853778fa1704a34
Ayush5304 <mishrayush0503@gmail.com>
2026-01-08 14:23:57 +0530

    [Ayush5304] Tablet java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day7/MediStore/Tablet.java
@@ -0,0 +1,15 @@
+package com.Day7.MediStore;
+
+import java.time.LocalDate;
+
+public class Tablet extends Medicine {
+
+    public Tablet(String name, double price, LocalDate expiryDate) {
+        super(name, price, expiryDate);
+    }
+
+    @Override
+    public boolean checkExpiry() {
+        return getExpiryDate().isBefore(LocalDate.now().minusMonths(1));
+    }
+}
