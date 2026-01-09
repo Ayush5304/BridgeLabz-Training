14c04db42f731f28a56ef0703dec939022595161
Ayush5304 <mishrayush0503@gmail.com>
2026-01-09 15:42:44 +0530

    [Ayush5304] FoodItemApp java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day8/FoodLoop/FoodItemApp.java
@@ -0,0 +1,14 @@
+package com.Day8.FoodLoop;
+
+public class FoodItemApp {
+    public static void main(String[] args) {
+
+        FoodItem paneer = new VegItem("Paneer Butter Masala", 300, 5);
+        FoodItem burger = new NonVeg("Chicken Burger", 250, 3);
+        FoodItem fries  = new VegItem("French Fries", 150, 10);
+
+        Order order = new Order(paneer, burger, fries);
+
+        order.placeOrder();
+    }
+}
