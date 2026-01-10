eadb068916bda77a3e11a7404663492fbe122b60
Ayush5304 <mishrayush0503@gmail.com>
2026-01-10 16:01:23 +0530

    [Ayush5304]  HospitalUnit java code

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioPractice/src/com/Day1/AmbulanceRoute/HospitalUnit.java
@@ -0,0 +1,13 @@
+package com.Day1.AmbulanceRoute;
+
+public class HospitalUnit {
+    String name;
+    boolean available;
+    HospitalUnit next;
+
+    public HospitalUnit(String name, boolean available) {
+        this.name = name;
+        this.available = available;
+        this.next = null;
+    }
+}
