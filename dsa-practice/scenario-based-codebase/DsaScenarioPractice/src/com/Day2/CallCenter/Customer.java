0ca155511562c3ca2ca0a43d956a78ef97e056c3
Ayush5304 <mishrayush0503@gmail.com>
2026-01-12 16:51:19 +0530

    [Ayush5304]Customer java code

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioPractice/src/com/Day2/CallCenter/Customer.java
@@ -0,0 +1,11 @@
+package com.Day2.CallCenter;
+
+class Customer {
+    String customerId;
+    boolean isVIP;
+
+    Customer(String customerId, boolean isVIP) {
+        this.customerId = customerId;
+        this.isVIP = isVIP;
+    }
+}
