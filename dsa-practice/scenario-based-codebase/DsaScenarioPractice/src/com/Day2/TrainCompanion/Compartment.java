4503391e6aad383474c68815ba54f1599b4c38ab
Ayush5304 <mishrayush0503@gmail.com>
2026-01-12 16:52:44 +0530

    [Ayush5304] Compartment java code

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioPractice/src/com/Day2/TrainCompanion/Compartment.java
@@ -0,0 +1,13 @@
+package com.Day2.TrainCompanion;
+
+public class Compartment {
+    String name;
+    Compartment prev;
+    Compartment next;
+
+    public Compartment(String name) {
+        this.name = name;
+        this.prev = null;
+        this.next = null;
+    }
+}
