279ce3e128b8800527b19e0ce604e842265103a2
Ayush5304 <mishrayush0503@gmail.com>
2026-01-13 13:10:09 +0530

    [Ayush5304] Customer java code

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioPractice/src/com/Day3/SmartCheckout/Customer.java
@@ -0,0 +1,12 @@
+package com.Day3.SmartCheckout;
+
+import java.util.*;
+public class Customer {
+    String  name;
+    List<String>items;
+	public Customer( String  name, List<String>items) {
+	this.name=name;
+	this.items=items;
+	}
+
+}
