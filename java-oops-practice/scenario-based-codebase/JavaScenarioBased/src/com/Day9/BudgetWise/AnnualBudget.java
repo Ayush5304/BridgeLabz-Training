7790d23c15e0a7a2bdb26cfb43fb5db4167dfb72
Ayush5304 <mishrayush0503@gmail.com>
2026-01-13 11:22:06 +0530

    [Ayush5304] AnnualBudget java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day9/BudgetWise/AnnualBudget.java
@@ -0,0 +1,21 @@
+package com.Day9.BudgetWise;
+ import java.util.*;
+class AnnualBudget extends Budget {
+
+    public AnnualBudget(double income, Map<String, Double> categoryLimits) {
+        super(income, categoryLimits);
+    }
+
+    @Override
+    public void generateReport() {
+        System.out.println("📈 Annual Budget Summary");
+        System.out.println("Annual Income: " + income);
+        System.out.println("Annual Expenses: " + getTotalExpenses());
+        System.out.println("Annual Savings: " + calculateNetSavings());
+    }
+
+    @Override
+    public void detectOverspend() {
+        System.out.println("Annual overspend analysis completed.");
+    }
+}
