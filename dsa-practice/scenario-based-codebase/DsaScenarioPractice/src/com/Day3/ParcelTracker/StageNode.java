89bcdf144889a5c5a371a8f01d10916ebfc4a010
Ayush5304 <mishrayush0503@gmail.com>
2026-01-13 13:12:23 +0530

    [Ayush5304] StageNode java code

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioPractice/src/com/Day3/ParcelTracker/StageNode.java
@@ -0,0 +1,11 @@
+package com.Day3.ParcelTracker;
+
+public class StageNode {
+   String stage;
+   StageNode next;
+	public StageNode(String stage) {
+   this.stage=stage;
+   this.next=null;
+	}
+}
+ 
