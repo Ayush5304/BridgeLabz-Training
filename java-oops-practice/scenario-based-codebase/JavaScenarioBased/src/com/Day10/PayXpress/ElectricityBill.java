9930d2a881b509221495978217d5342b41aa29e5
Ayush5304 <mishrayush0503@gmail.com>
2026-01-14 21:38:10 +0530

    [Ayush5304]  ElectricityBill java code

---
+++ java-oops-practice/scenario-based-codebase/JavaScenarioBased/src/com/Day10/PayXpress/ElectricityBill.java
@@ -0,0 +1,15 @@
+package com.Day10.PayXpress;
+import java.time.LocalDate; 
+class ElectricityBill extends Bill {
+
+    public ElectricityBill(double amount, LocalDate dueDate) {
+        super("Electricity", amount, dueDate);
+    }
+
+    @Override
+    public void sendReminder() {
+        if (!isPaid()) {
+            System.out.println("⚡ Electricity Bill Reminder: Pay before " + dueDate);
+        }
+    }
+}
