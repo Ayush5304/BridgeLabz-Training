d84237ab255365cfd33127efbaa4af302af390a1
Ayush5304 <mishrayush0503@gmail.com>
2026-01-16 21:33:19 +0530

    [Ayush5304] BankingMain java code

---
+++ java-collections-practice/gcr-codebase/JavaCollectionsPractice/src/com/Collections/MapInterface/BankingSystem/BankingMain.java
@@ -0,0 +1,21 @@
+package com.Collections.MapInterface.BankingSystem;
+
+public class BankingMain {
+
+    public static void main(String[] args) {
+
+        BankingSystem bank = new BankingSystem();
+
+        bank.createAccount(101, 5000);
+        bank.createAccount(102, 3000);
+        bank.createAccount(103, 8000);
+
+        bank.requestWithdrawal(101);
+        bank.requestWithdrawal(102);
+        bank.requestWithdrawal(103);
+
+        bank.processWithdrawals(4000);
+
+        bank.showAccountsSortedByBalance();
+    }
+}
