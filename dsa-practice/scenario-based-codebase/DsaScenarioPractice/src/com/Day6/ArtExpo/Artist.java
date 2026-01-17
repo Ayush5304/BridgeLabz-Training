1715c2c7d9b6eb9eebc9812cbd5287c6c2d63d52
Ayush5304 <mishrayush0503@gmail.com>
2026-01-17 16:16:29 +0530

    [Ayush5304]  Artist java code

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioPractice/src/com/Day6/ArtExpo/Artist.java
@@ -0,0 +1,16 @@
+package com.Day6.ArtExpo;
+
+public class Artist {
+    String name;
+    int registrationTime;   
+
+    public Artist(String name, int registrationTime) {
+        this.name = name;
+        this.registrationTime = registrationTime;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + registrationTime + ")";
+    }
+}
