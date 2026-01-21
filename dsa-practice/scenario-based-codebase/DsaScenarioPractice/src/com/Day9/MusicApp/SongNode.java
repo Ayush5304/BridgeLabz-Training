6ff96f844cb46473d4c5b9f7624ae5dc7a9e60ea
Ayush5304 <mishrayush0503@gmail.com>
2026-01-21 16:25:52 +0530

    [Ayush5304] SongNode java code

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioPractice/src/com/Day9/MusicApp/SongNode.java
@@ -0,0 +1,16 @@
+package com.Day9.MusicApp;
+
+class SongNode {
+    int trackId;
+    String title;
+    String artist;
+
+    SongNode left, right;
+
+    SongNode(int trackId, String title, String artist) {
+        this.trackId = trackId;
+        this.title = title;
+        this.artist = artist;
+        left = right = null;
+    }
+}
