95d6bcec59f487a393d62ec3d2ffc33c9eb1e92e
Ayush5304 <mishrayush0503@gmail.com>
2026-01-21 16:24:41 +0530

    [Ayush5304]Product java code

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioPractice/src/com/Day9/ECommerceProductInventory/Product.java
@@ -0,0 +1,13 @@
+package com.Day9.ECommerceProductInventory;
+
+class Product {
+    int sku;
+    String name;
+    double price;
+
+    Product(int sku, String name, double price) {
+        this.sku = sku;
+        this.name = name;
+        this.price = price;
+    }
+}
