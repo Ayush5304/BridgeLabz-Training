9f7413651c6b036b77018cea54968a4af9b25a03
Ayush5304 <mishrayush0503@gmail.com>
2026-01-24 16:03:13 +0530

    [Ayush5304]Promotion java code

---
+++ java-collections-practice/scenario-based-codebase/CollectionsScenarioPractice/src/com/Day2/DealTracker/Promotion.java
@@ -0,0 +1,12 @@
+package com.Day2.DealTracker;
+
+public abstract class Promotion<T> {
+    protected T type;
+
+    public Promotion(T type) {
+        this.type = type;
+    }
+
+    public abstract String getCode();
+    public abstract double getValue();
+}
