ffe62f547345266268aa024fd4c6676cdd3d90ad
Ayush5304 <mishrayush0503@gmail.com>
2026-01-27 19:34:02 +0530

    [Ayush5304] FilterJson java code

---
+++ java-iostreams-practice/gcr-codebase/JavaIOStreamsPractice/src/com/JSON/PracticeProblems/FilterJson.java
@@ -0,0 +1,24 @@
+package com.JSON.PracticeProblems;
+
+import com.google.gson.*;
+import java.io.*;
+
+public class FilterJson {
+    public static void main(String[] args) throws Exception {
+
+        JsonArray arr = JsonParser.parseReader(
+                new FileReader("people.json")
+        ).getAsJsonArray();
+
+        System.out.println("People with age > 25:\n");
+
+        for (JsonElement e : arr) {
+            JsonObject obj = e.getAsJsonObject();
+
+            if (obj.get("age").getAsInt() > 25) {
+                System.out.println(obj);
+            }
+        }
+    }
+}
+
