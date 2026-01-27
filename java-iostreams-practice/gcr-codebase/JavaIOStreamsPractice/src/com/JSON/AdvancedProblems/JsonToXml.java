66544e137f9b87f192dd33c18f935afcd834a8d4
Ayush5304 <mishrayush0503@gmail.com>
2026-01-27 19:36:53 +0530

    [Ayush5304] JsonToXml java code

---
+++ java-iostreams-practice/gcr-codebase/JavaIOStreamsPractice/src/com/JSON/AdvancedProblems/JsonToXml.java
@@ -0,0 +1,18 @@
+package com.JSON.AdvancedProblems;
+
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import java.io.*;
+
+public class JsonToXml {
+    public static void main(String[] args) throws Exception {
+
+        ObjectMapper jsonMapper = new ObjectMapper();
+        XmlMapper xmlMapper = new XmlMapper();
+
+        JsonNode node = jsonMapper.readTree(new File("data.json"));
+        String xml = xmlMapper.writeValueAsString(node);
+
+        System.out.println(xml);
+    }
+}
