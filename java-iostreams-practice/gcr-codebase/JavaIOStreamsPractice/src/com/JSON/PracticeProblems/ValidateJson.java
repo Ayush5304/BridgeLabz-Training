f94d61260eba45417c75a9fe0aa487e0d7a301ad
Ayush5304 <mishrayush0503@gmail.com>
2026-01-27 19:35:00 +0530

    [Ayush5304] ValidateJson java code

---
+++ java-iostreams-practice/gcr-codebase/JavaIOStreamsPractice/src/com/JSON/PracticeProblems/ValidateJson.java
@@ -0,0 +1,19 @@
+package com.JSON.PracticeProblems;
+
+import com.fasterxml.jackson.databind.*;
+import java.io.*;
+
+
+public class ValidateJson {
+    public static void main(String[] args) {
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            mapper.readTree(new File("data.json"));
+            System.out.println("Valid JSON Structure");
+        } catch (Exception e) {
+            System.out.println("Invalid JSON ");
+        }
+    }
+}
