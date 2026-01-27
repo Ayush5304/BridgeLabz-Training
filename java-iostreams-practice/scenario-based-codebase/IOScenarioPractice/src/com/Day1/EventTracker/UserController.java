e2fad0b4419a066d7d5c0bc7ca27a0522be99365
Ayush5304 <mishrayush0503@gmail.com>
2026-01-27 19:43:51 +0530

    [Ayush5304] UserController java code

---
+++ java-iostreams-practice/scenario-based-codebase/IOScenarioPractice/src/com/Day1/EventTracker/UserController.java
@@ -0,0 +1,15 @@
+package com.Day1.EventTracker;
+
+public class UserController {
+
+    @AuditTrail(action = "USER_LOGIN")
+    public void login() {}
+
+    @AuditTrail(action = "FILE_UPLOAD")
+    public void uploadFile() {}
+
+    @AuditTrail(action = "RECORD_DELETE")
+    public void deleteRecord() {}
+
+    public void viewProfile() {}  
+}
