f3cfcdbd73dfd4c8e272921c2626f6e40fb3a30a
Ayush5304 <mishrayush0503@gmail.com>
2026-01-27 19:33:43 +0530

    [Ayush5304] CreateJson java code

---
+++ java-iostreams-practice/gcr-codebase/JavaIOStreamsPractice/src/com/JSON/PracticeProblems/CreateJson.java
@@ -0,0 +1,21 @@
+package com.JSON.PracticeProblems;
+
+import com.google.gson.*;
+
+public class CreateJson {
+    public static void main(String[] args) {
+
+        JsonObject student = new JsonObject();
+        student.addProperty("name", "Ayush");
+        student.addProperty("age", 21);
+
+        JsonArray subjects = new JsonArray();
+        subjects.add("Math");
+        subjects.add("Physics");
+        subjects.add("Computer Science");
+
+        student.add("subjects", subjects);
+
+        System.out.println(student.toString());
+    }
+}
