73a975cb5d4ef7f05b5882b0e792eb2790ddd374
Ayush5304 <mishrayush0503@gmail.com>
2026-01-27 19:42:11 +0530

    [Ayush5304] AuditLog java code

---
+++ java-iostreams-practice/scenario-based-codebase/IOScenarioPractice/src/com/Day1/EventTracker/AuditLog.java
@@ -0,0 +1,21 @@
+package com.Day1.EventTracker;
+
+
+import java.time.LocalDateTime;
+
+public class AuditLog {
+
+    private String event;
+    private String className;
+    private String methodName;
+    private LocalDateTime timestamp;
+
+    public AuditLog(String event, String className,
+                    String methodName, LocalDateTime timestamp) {
+        this.event = event;
+        this.className = className;
+        this.methodName = methodName;
+        this.timestamp = timestamp;
+    }
+}
+
