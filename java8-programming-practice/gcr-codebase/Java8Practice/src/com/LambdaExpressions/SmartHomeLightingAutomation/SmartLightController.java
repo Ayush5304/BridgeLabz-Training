ab333a98714cb76702358b4c8d6b6bddf3c780bb
Ayush5304 <mishrayush0503@gmail.com>
2026-01-29 21:14:52 +0530

    [Ayush5304] SmartLightController java code

---
+++ java8-programming-practice/gcr-codebase/Java8Practice/src/com/LambdaExpressions/SmartHomeLightingAutomation/SmartLightController.java
@@ -0,0 +1,8 @@
+package com.LambdaExpressions.SmartHomeLightingAutomation;
+
+public class SmartLightController {
+
+    public void execute(LightBehavior behavior) {
+        behavior.activate();
+    }
+}
