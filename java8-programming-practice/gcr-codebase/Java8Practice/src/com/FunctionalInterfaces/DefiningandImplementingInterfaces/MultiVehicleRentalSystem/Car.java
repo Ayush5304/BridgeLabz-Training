3c63cc97803dc7dde977e77a4d8ac8bfa64fa528
Ayush5304 <mishrayush0503@gmail.com>
2026-01-29 20:57:48 +0530

    [Ayush5304] Car java code

---
+++ java8-programming-practice/gcr-codebase/Java8Practice/src/com/FunctionalInterfaces/DefiningandImplementingInterfaces/MultiVehicleRentalSystem/Car.java
@@ -0,0 +1,28 @@
+package com.FunctionalInterfaces.DefiningandImplementingInterfaces.MultiVehicleRentalSystem;
+
+class Car implements Rentable {
+    public void rent() {
+        System.out.println("Car rented ");
+    }
+    public void returnVehicle() {
+        System.out.println("Car returned ");
+    }
+}
+
+class Bike implements Rentable {
+    public void rent() {
+        System.out.println("Bike rented ");
+    }
+    public void returnVehicle() {
+        System.out.println("Bike returned ");
+    }
+}
+
+class Bus implements Rentable {
+    public void rent() {
+        System.out.println("Bus rented ");
+    }
+    public void returnVehicle() {
+        System.out.println("Bus returned ");
+    }
+}
