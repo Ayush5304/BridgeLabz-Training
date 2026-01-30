3573f5accc838eb0c448d64873e0bb5e33082fdc
Ayush5304 <mishrayush0503@gmail.com>
2026-01-30 18:41:28 +0530

    [Ayush5304] AvgSalaryByGender java code

---
+++ java8-programming-practice/scenario-based-codebase/Java8ScenarioPractice/src/com/StreamScenariosProblems/AvgSalaryByGender.java
@@ -0,0 +1,28 @@
+package com.StreamScenariosProblems;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class AvgSalaryByGender {
+
+    public static void main(String[] args) {
+
+        List<Employee> employees = getEmployees();
+
+        Map<String, Double> avgSalary =
+                employees.stream()
+                        .collect(Collectors.groupingBy(
+                                Employee::getGender,
+                                Collectors.averagingDouble(Employee::getSalary)
+                        ));
+
+        avgSalary.forEach((k, v) -> System.out.println(k + " → " + v));
+    }
+
+    private static List<Employee> getEmployees() {
+        return Arrays.asList(
+                new Employee(1,"Ayush",23,"Male","IT",2019,60000),
+                new Employee(2,"Neha",25,"Female","HR",2018,55000),
+                new Employee(3,"Rohit",28,"Male","Finance",2016,75000)
+        );
+    }
+}
