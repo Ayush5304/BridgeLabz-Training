0433897b1e2093d08c30a3865b9e1bd37eb0edfd
Ayush5304 <mishrayush0503@gmail.com>
2026-01-30 18:40:27 +0530

    [Ayush5304] AverageAgeByGender java code

---
+++ java8-programming-practice/scenario-based-codebase/Java8ScenarioPractice/src/com/StreamScenariosProblems/AverageAgeByGender.java
@@ -0,0 +1,33 @@
+package com.StreamScenariosProblems;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class AverageAgeByGender {
+
+    public static void main(String[] args) {
+
+        List<Employee> employees = getEmployees();
+
+        Map<String, Double> avgAge =
+                employees.stream()
+                        .collect(Collectors.groupingBy(
+                                Employee::getGender,
+                                Collectors.averagingInt(Employee::getAge)
+                        ));
+
+        avgAge.forEach((k, v) -> System.out.println(k + " → " + v));
+    }
+
+    private static List<Employee> getEmployees() {
+        return Arrays.asList(
+                new Employee(1,"Ayush",23,"Male","IT",2019,60000),
+                new Employee(2,"Neha",25,"Female","HR",2018,55000),
+                new Employee(3,"Rohit",28,"Male","Finance",2016,75000),
+                new Employee(4,"Pooja",22,"Female","IT",2021,50000),
+                new Employee(5,"Karan",30,"Male","HR",2014,80000),
+                new Employee(6,"Anjali",27,"Female","Finance",2017,72000),
+                new Employee(7,"Amit",35,"Male","Management",2012,90000)
+        );
+    }
+}
