5145276d9581d585bcf1e390e7705c1eaf61e2cd
Ayush5304 <mishrayush0503@gmail.com>
2026-01-30 18:45:44 +0530

    [Ayush5304]FirstFiveLowestPaid java code

---
+++ java8-programming-practice/scenario-based-codebase/Java8ScenarioPractice/src/com/StreamScenariosProblems/FirstFiveLowestPaid.java
@@ -0,0 +1,28 @@
+package com.StreamScenariosProblems;
+
+import java.util.*;
+
+public class FirstFiveLowestPaid {
+
+    public static void main(String[] args) {
+
+        List<Employee> employees = getEmployees();
+
+        employees.stream()
+                .sorted(Comparator.comparing(Employee::getSalary))
+                .limit(5)
+                .forEach(System.out::println);
+    }
+
+    private static List<Employee> getEmployees() {
+        return Arrays.asList(
+                new Employee(1,"Ayush",23,"Male","IT",2019,60000),
+                new Employee(2,"Neha",25,"Female","HR",2018,55000),
+                new Employee(3,"Rohit",28,"Male","Finance",2016,75000),
+                new Employee(4,"Pooja",22,"Female","IT",2021,50000),
+                new Employee(5,"Karan",30,"Male","HR",2014,80000),
+                new Employee(6,"Anjali",27,"Female","Finance",2017,72000),
+                new Employee(7,"Amit",35,"Male","Management",2012,90000)
+        );
+    }
+}
