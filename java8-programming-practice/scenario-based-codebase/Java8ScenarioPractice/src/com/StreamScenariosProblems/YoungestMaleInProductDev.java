ff87a2172633050dc31d8682c562a6549e551ce3
Ayush5304 <mishrayush0503@gmail.com>
2026-01-31 15:33:28 +0530

    [Ayush5304] YoungestMaleInProductDev.java code

--- java8-programming-practice/scenario-based-codebase/Java8ScenarioPractice/src/com/StreamScenariosProblems/YoungestMaleInProductDev.java
+++ java8-programming-practice/scenario-based-codebase/Java8ScenarioPractice/src/com/StreamScenariosProblems/YoungestMaleInProductDev.java
@@ -20,9 +20,13 @@ public class YoungestMaleInProductDev {
 
     private static List<Employee> getEmployees() {
         return Arrays.asList(
-                new Employee(1,"Ayush",23,"Male","Product Development",2019,60000),
-                new Employee(2,"Rohit",28,"Male","Product Development",2016,75000),
-                new Employee(3,"Amit",35,"Male","Management",2012,90000)
+                new Employee(1,"Ayush",23,"Male","IT",2019,60000),
+                new Employee(2,"Neha",25,"Female","HR",2018,55000),
+                new Employee(3,"Rohit",28,"Male","Finance",2016,75000),
+                new Employee(4,"Pooja",22,"Female","IT",2021,50000),
+                new Employee(5,"Karan",30,"Male","HR",2014,80000),
+                new Employee(6,"Anjali",27,"Female","Finance",2017,72000),
+                new Employee(7,"Amit",35,"Male","Management",2012,90000)
         );
     }
 }
