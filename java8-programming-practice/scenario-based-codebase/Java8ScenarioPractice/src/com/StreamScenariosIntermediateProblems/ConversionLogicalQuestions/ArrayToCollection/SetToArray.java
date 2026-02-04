e5ae2939aff07bca4f591f2072233cd10f644cc1
Ayush5304 <mishrayush0503@gmail.com>
2026-02-04 15:36:39 +0530

    [Ayush5304]  ConversionLogicalQuestions added

---
+++ java8-programming-practice/scenario-based-codebase/Java8ScenarioPractice/src/com/StreamScenariosIntermediateProblems/ConversionLogicalQuestions/ArrayToCollection/SetToArray.java
@@ -0,0 +1,14 @@
+package com.StreamScenariosIntermediateProblems.ConversionLogicalQuestions.ArrayToCollection;
+
+import java.util.*;
+
+public class SetToArray {
+    public static void main(String[] args) {
+
+        Set<Integer> set = Set.of(1, 2, 3);
+
+        Integer[] arr = set.toArray(new Integer[0]);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ java8-programming-practice/scenario-based-codebase/Java8ScenarioPractice/src/com/StreamScenariosIntermediateProblems/ConversionLogicalQuestions/ListToSet/SetToReverseList.java
@@ -0,0 +1,16 @@
+package com.StreamScenariosIntermediateProblems.ConversionLogicalQuestions.ListToSet;
+
+import java.util.*;
+
+public class SetToReverseList {
+    public static void main(String[] args) {
+
+        Set<Integer> set = Set.of(1, 2, 3, 4);
+
+        List<Integer> list = set.stream()
+                                .sorted(Comparator.reverseOrder())
+                                .toList();
+
+        System.out.println(list);
+    }
+}
