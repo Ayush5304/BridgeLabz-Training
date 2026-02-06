3728098d71b9c89ff6d2bb7e502651d97f2da18b
Ayush5304 <mishrayush0503@gmail.com>
2026-02-06 20:58:26 +0530

    [Ayush5304] CreatorStats java code

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioPractice/src/com/AssessmentProblems/StreamBuzz/CreatorStats.java
@@ -0,0 +1,24 @@
+package com.AssessmentProblems.StreamBuzz;
+
+import java.util.*;
+
+public class CreatorStats {
+
+    private String creatorName;
+    private double[] weeklyLikes;
+
+    public static List<CreatorStats> EngagementBoard = new ArrayList<>();
+
+    public CreatorStats(String creatorName, double[] weeklyLikes) {
+        this.creatorName = creatorName;
+        this.weeklyLikes = weeklyLikes;
+    }
+
+    public String getCreatorName() {
+        return creatorName;
+    }
+
+    public double[] getWeeklyLikes() {
+        return weeklyLikes;
+    }
+}
