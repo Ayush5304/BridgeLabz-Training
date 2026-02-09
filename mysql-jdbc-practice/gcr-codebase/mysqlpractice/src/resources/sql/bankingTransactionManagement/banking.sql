3df13b38949b94f44f7a5d30a3f9f6a8100e7d09
Ayush5304 <mishrayush0503@gmail.com>
2026-02-09 12:37:22 +0530

    [Ayush5304] banking sql code

--- mysql-jdbc-practice/gcr-codebase/mysqlpractice/src/resources/sql/bankingTransactionManagement/banking.sql
+++ mysql-jdbc-practice/gcr-codebase/mysqlpractice/src/resources/sql/bankingTransactionManagement/banking.sql
@@ -10,3 +10,31 @@ CREATE TABLE transaction_history (
     amount DOUBLE,
     tx_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+mysql> INSERT INTO account VALUES
+    -> (101,'Rahul',5000),
+    -> (102,'Amit',3000),
+    -> (103,'Neha',8000);
+    
+Query OK, 3 rows affected (0.01 sec)
+Records: 3  Duplicates: 0  Warnings: 0
+
+mysql> SELECT * FROM account;
++------------+-------+---------+
+| account_id | name  | balance |
++------------+-------+---------+
+|        101 | Rahul |    5000 |
+|        102 | Amit  |    3000 |
+|        103 | Neha  |    8000 |
++------------+-------+---------+
+3 rows in set (0.00 sec)
+
+mysql> START TRANSACTION;
+Query OK, 0 rows affected (0.00 sec)
+
+
+mysql> UPDATE account
+    -> SET balance = balance - 15000
+    -> WHERE account_id = 1;
+    
+Query OK, 0 rows affected (0.00 sec)
+Rows matched: 0  Changed: 0  Warnings: 0
