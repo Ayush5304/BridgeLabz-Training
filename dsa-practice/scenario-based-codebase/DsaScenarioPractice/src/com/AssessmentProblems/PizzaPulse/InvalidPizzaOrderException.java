cb192a35e5eb9e6d678bfc2e2e2e8a7191f109e1
Ayush5304 <mishrayush0503@gmail.com>
2026-02-18 16:17:22 +0530

    [Ayush5304] added PizzaPulse/

---
+++ dsa-practice/scenario-based-codebase/DsaScenarioPractice/src/com/AssessmentProblems/PizzaPulse/InvalidPizzaOrderException.java
@@ -0,0 +1,7 @@
+package com.AssessmentProblems.PizzaPulse;
+
+public class InvalidPizzaOrderException extends Exception{
+	public InvalidPizzaOrderException(String message){		
+	super(message);
+	}
+}
---
+++ dsa-practice/scenario-based-codebase/DsaScenarioPractice/src/com/AssessmentProblems/PizzaPulse/UserInterface.java
@@ -0,0 +1,36 @@
+package com.AssessmentProblems.PizzaPulse;
+
+import java.util.*;
+public class UserInterface {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		PizzaUtil pu=new PizzaUtil();
+		try {
+		System.out.println("Enter pizza order details");
+		String input = sc.nextLine();
+		
+		String[] parts= input.split(":");
+		
+		if(parts.length!=4) {
+			System.out.println("invalid input");
+			return;
+		}
+		String orderId= parts[0];
+		String pizzaType= parts[1];
+		double basePrice= Double.parseDouble(parts[2]);
+		int quantity= Integer.parseInt(parts[3]);
+		
+		pu.validateOrderId(orderId);
+		pu.validatePizzaType(pizzaType);
+		pu.validateQuantity(quantity);
+		 double output= pu.calculateFinalBill(pizzaType, basePrice, quantity);
+		  System.out.println(
+	              "Total bill for " + pizzaType + " pizzas: " + output);
+	        }
+	
+   catch(InvalidPizzaOrderException e) {
+	   System.out.println(e.getMessage());
+   }
+	}
+}
